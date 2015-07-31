9f275538fd58e989ec8cdbdd519bacdb7c1e5363
vedantswain <vedant12118@iiitd.ac.in>
2015-07-31 14:27:33 +0530

    table ready

--- popup/popup.js
+++ popup/popup.js
@@ -68,7 +68,7 @@ function loadStatusItems(tx, rs) {
 function renderStatus(row) {
 	// console.log("Status");
 	console.log(row);
-   	li="<li >" + row.status+","+row.added_on+","+row.nudge+","+row.reaction+","+row.duration + "</li>";
+   	li="<tr>" +"<td>"+ row.status+"</td><td>"+row.added_on+"</td><td>"+row.nudge+"</td><td>"+row.reaction+"</td><td>"+row.duration + "</td></tr>";
    	// "<button id=item_"+row.ID+">Delete</button>
 
 	return li;
